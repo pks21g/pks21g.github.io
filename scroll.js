e3bd21ebe52a84ff125612540a901788c8b7a056
pks21g <paulsmith414@yahoo.com>
2023-12-08 09:10:05 -0600

    adding new protfolio website

---
+++ scroll.js
@@ -0,0 +1,28 @@
+
+const arrow = document.getElementById('arrow');
+
+window.onscroll = function() { scrollFunction()};
+
+const scrollFunction = () =>{
+    if (document.body.scrollTop > 20 || document.documentElement < 20)
+        arrow.style.display = 'block';
+    else
+        arrow.style.display = 'none';
+}
+
+arrow.addEventListener('click', () => {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+})
+
+   
+function handleClick(e) {
+    var elmnt = document.getElementById(e);
+    elmnt.scrollIntoView({
+        behavior: "smooth", 
+        block: "start",
+        inline: "nearest"
+    });
+  }
+
+
