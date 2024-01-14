a8a55c818e850792d970f00f835e44ddfd046abd
pks21g <paulsmith414@yahoo.com>
2024-01-13 22:21:37 -0600

    Revert "changed secondary screen color"
    This reverts commit 2daed2059f6b3597f3f1a2dde493967a5786a760.
    
    fixing disco-fighter

--- photography-website/script.js
+++
@@ -1,9 +0,0 @@
-
-const sel = document.querySelector(".services");
-const act = document.querySelector(".service-opt");
-sel.addEventListener("click", () => {
-  act.classList.toggle("active");
-});
-
-
-
